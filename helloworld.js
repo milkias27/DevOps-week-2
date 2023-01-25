de82ac3c46d1ad1e4796b84885095a8c8b9cd7b0
milkias27 <mhaile1@live.maryville.edu>
2023-01-25 13:16:43 -0800

    ...
    ...

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello MU DevOps Class\n')
+   response.end('Hello MU  DEVOPS Class 2022\n')
 }).listen(3333)
 
 // Console will print the message
